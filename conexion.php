8f5c5c318c716c0d3ee5127ca835db14dca3d5b0
delpiero ramos ramos <delpiee@hotmail.com>
2019-09-08 23:42:00 -0500

    commit standar

--- conexion.php
+++ conexion.php
@@ -1,16 +1,17 @@
 <?php
+/*
  $host="ec2-174-129-27-158.compute-1.amazonaws.com";
-	$User="ascbovghbpnyir";
-	$Password="e6a76c664ecdaf92eedd61ca71b44201e32782c6e77f36988bb17bf078e784ce";
-	$Database="d5jcl6jn7055pr";
-	$Port="5432";
+	$user="ascbovghbpnyir";
+	$password="e6a76c664ecdaf92eedd61ca71b44201e32782c6e77f36988bb17bf078e784ce";
+	$database="d5jcl6jn7055pr";
+	$port="5432";
 
-	 $conexion=new mysqli($host,$User,$Password,$Database,$Port); 
+	 $conexion=new mysqli($host,$user,$password,$database,$port); 
 	
 
      if ($conexion->connect_errno) 
      {
  	    die("fallo de la conexion");
  	 }
-
+*/
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php  
-
+/*
 include "conexion.php";
 $consulta="SELECT * FROM SPORTS";
 $sql=mysqli_query($conexion,$consulta);
@@ -13,7 +13,9 @@ if ($result==0) {
 			 $description=$data["descripcion"];
 			 $history=$data["historia"];
 			}
-		}
+
+	}
+	*/	
 ?>
 
 
@@ -39,10 +41,16 @@ if ($result==0) {
   <div class="col-4">
     <div class="list-group" id="list-tab" role="tablist">
 
-      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home"><?php echo $name; ?></a>
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">ATLETISMO</a>
       <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Baloncesto</a>
       <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Boxeo</a>
       <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Canotaje velocidad</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Ciclismo Bmx</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Esgrima</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Futbol</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Gimnasia artistica</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Golf</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">judo</a>
 
 
     </div>
@@ -52,15 +60,22 @@ if ($result==0) {
       <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
       	<div class="colum_description">
       		<h4>Descripcion</h4>
-      		<p><?php echo $description; ?></p>
+      		<p>Lima 2019 reunirá a un máximo de 740 competidores, entre masculinos y femeninos en todas las pruebas. El país más destacado en atletismo en la historia de los Juegos Panamericanos es Estados Unidos, que suma 709 medallas, de las cuales 292 son de oro. La sede central de atletismo será el Estadio Atlético, de la Villa Deportiva Nacional (Videna). Tan solo la maratón y marcha atlética se desarrollarán en un circuito en el distrito de Miraflores que tendrá como inicio y final el emblemático Parque Kennedy.</p>
       		<h4>Historia</h4>
-      		<p><?php echo $history; ?></p>
+      		<p>El atletismo es la forma organizada más antigua de deporte y se viene celebrando desde hace miles de años. Las primeras reuniones organizadas de la historia fueron los Juegos Olímpicos, que iniciaron los griegos en el año 776 a.C. El atletismo posteriormente adquirió un gran seguimiento en Europa y América.</p>
       	</div>
 
 
 
       </div>
-      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">2</div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+      	<div class="colum_description">
+      		<h4>Descripcion</h4>
+      		<p>Se enfrentan dos equipos de cinco jugadores, en una cancha de 28 metros de largo por 15 de ancho. Gana el equipo que anote más puntos introduciendo el balón en la canasta del equipo contrario, ubicada a 3,05 metros del suelo.</p>
+      		<h4>Historia</h4>
+      		<p>Su creación ha sido atribuida al canadiense James Naismith en 1891, como método para ejercitar a sus alumnos en Nueva Inglaterra, Estados Unidos. Originalmente utilizó dos canastas de duraznos, y se cambiaron en 1906, por aros de metal con tableros. En la actualidad es uno de los deportes más populares en el mundo.</p>
+      	</div>
+      </div>
       <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">3</div>
       <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
     </div>
